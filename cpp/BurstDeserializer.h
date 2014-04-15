73c3ee5cb07ad9e82b1ca4fe79e847c906089d19
Ryan Bauman <ryan.bauman@axiosengineering.com>
2014-04-15 17:40:47 -0400

    REDHAWK bulkioInterfaces - 1.10.0-rc5

---
+++ cpp/BurstDeserializer.h
@@ -0,0 +1,32 @@
+/*
+ * This file is protected by Copyright. Please refer to the COPYRIGHT file distributed with this
+ * source distribution.
+ *
+ * This file is part of REDHAWK Basic Components BurstDeserializer.
+ *
+ * REDHAWK Basic Components AmFmPmBasebandDemod is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * REDHAWK Basic Components AmFmPmBasebandDemod is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program.  If not, see http://www.gnu.org/licenses/.
+ */
+#ifndef BURSTDESERIALIZER_IMPL_H
+#define BURSTDESERIALIZER_IMPL_H
+
+#include "BurstDeserializer_base.h"
+
+class BurstDeserializer_i : public BurstDeserializer_base
+{
+    ENABLE_LOGGING
+    public:
+        BurstDeserializer_i(const char *uuid, const char *label);
+        ~BurstDeserializer_i();
+        int serviceFunction();
+};
+
+#endif
